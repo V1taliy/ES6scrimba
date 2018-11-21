cd4957130f5299fa89691ab40731945515d188ca
Vitaliy Sakhno <vitaliy.sakhno1@gmail.com>
2018-11-21 14:39:35 +0200

    add destructor

---
+++ destructor.js
@@ -0,0 +1,19 @@
+// Object
+/*const personalInformation = {
+    firstName: 'Dylan',
+    lastName: 'Israel',
+    city: 'Austin',
+    state: 'Texas',
+    zipCode: 73301
+};
+
+const {firstName: fn, lastName: ln} = personalInformation;
+
+console.log(`${fn} ${ln}`);*/
+
+//Arrays
+let [firstName, middleName, lastName] = ['Ololo', 'Coding God', 'Ukraine'];
+
+lastName = 'Clements';
+
+console.log(lastName);
