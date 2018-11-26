7422fb1a389d71882d5b881f6599eeb6ee40a053
Vitaliy Sakhno <vitaliy.sakhno1@gmail.com>
2018-11-26 15:38:30 +0200

    add async&await

---
+++ async&await.js
@@ -0,0 +1,41 @@
+const apiUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+
+async function getTop100Campers() {
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+    
+    console.log(json[0]);
+}
+
+// function getTop100Campers() {
+//     fetch(apiUrl)
+//     .then((r) => r.json())
+//     .then((json) => {
+//         console.log(json[0])
+//     }).catch((error) =>{
+//         console.log('failed');
+//     });
+// }
+
+getTop100Campers();
+
+//===================
+
+function resolveAfter3Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 3000);
+    });
+  }
+  
+  // resolveAfter3Seconds().then((data) => {
+  //     console.log(data);
+  // });
+  
+  async function getAsyncData() {
+      const result = await resolveAfter3Seconds();
+      console.log(result);
+  }
+  
+  getAsyncData();
